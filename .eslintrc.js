d11210ef8d3836c560690f49a2585b5696986e31
bxmmm <benjaminxh@gmail.com>
2017-01-08 13:11:24 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "rules": {
+    "strict": 0
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jquery": true
+  },
+
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "comma-dangle": [
+      "warn",
+      "never"
+    ],
+    "linebreak-style": [
+      "warn",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "warn",
+      "always"
+    ],
+    /* Advanced Rules*/
+    "no-unused-expressions": "warn",
+    "no-useless-concat": "warn",
+    "block-scoped-var": "error",
+    "consistent-return": "error"
+  }
+};
